0cff7528904f021fb6821d3432f6041356753b3d
Francisco Reyes <francisco@makingdevs.com>
2016-07-10 00:11:42 -0500

    Se agrega proyecto android con groovy

---
+++ RR/app/src/main/groovy/com/uaemex/rr/RR.groovy
@@ -0,0 +1,13 @@
+package com.uaemex.rr
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+class RR extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rr)
+    }
+}
