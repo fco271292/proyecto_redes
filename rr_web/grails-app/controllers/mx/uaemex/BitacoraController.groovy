ce76ecd4f7a7d0d62669c069fcc9436efe93caa1
Francisco Reyes <francisco@makingdevs.com>
2016-08-14 20:01:17 -0500

    #1 Se agrega controllers

---
+++ rr_web/grails-app/controllers/mx/uaemex/BitacoraController.groovy
@@ -0,0 +1,107 @@
+package mx.uaemex
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class BitacoraController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Bitacora.list(params), model:[bitacoraCount: Bitacora.count()]
+    }
+
+    def show(Bitacora bitacora) {
+        respond bitacora
+    }
+
+    def create() {
+        respond new Bitacora(params)
+    }
+
+    @Transactional
+    def save(Bitacora bitacora) {
+        if (bitacora == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (bitacora.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond bitacora.errors, view:'create'
+            return
+        }
+
+        bitacora.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'bitacora.label', default: 'Bitacora'), bitacora.id])
+                redirect bitacora
+            }
+            '*' { respond bitacora, [status: CREATED] }
+        }
+    }
+
+    def edit(Bitacora bitacora) {
+        respond bitacora
+    }
+
+    @Transactional
+    def update(Bitacora bitacora) {
+        if (bitacora == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (bitacora.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond bitacora.errors, view:'edit'
+            return
+        }
+
+        bitacora.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'bitacora.label', default: 'Bitacora'), bitacora.id])
+                redirect bitacora
+            }
+            '*'{ respond bitacora, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Bitacora bitacora) {
+
+        if (bitacora == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        bitacora.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'bitacora.label', default: 'Bitacora'), bitacora.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'bitacora.label', default: 'Bitacora'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ rr_web/grails-app/controllers/mx/uaemex/CareerController.groovy
@@ -0,0 +1,107 @@
+package mx.uaemex
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class CareerController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Career.list(params), model:[careerCount: Career.count()]
+    }
+
+    def show(Career career) {
+        respond career
+    }
+
+    def create() {
+        respond new Career(params)
+    }
+
+    @Transactional
+    def save(Career career) {
+        if (career == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (career.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond career.errors, view:'create'
+            return
+        }
+
+        career.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'career.label', default: 'Career'), career.id])
+                redirect career
+            }
+            '*' { respond career, [status: CREATED] }
+        }
+    }
+
+    def edit(Career career) {
+        respond career
+    }
+
+    @Transactional
+    def update(Career career) {
+        if (career == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (career.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond career.errors, view:'edit'
+            return
+        }
+
+        career.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'career.label', default: 'Career'), career.id])
+                redirect career
+            }
+            '*'{ respond career, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Career career) {
+
+        if (career == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        career.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'career.label', default: 'Career'), career.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'career.label', default: 'Career'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
