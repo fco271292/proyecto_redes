b7ff9eb02959f1b5aef90a60457edb22f8d3edb5
Francisco Reyes <francisco@makingdevs.com>
2016-08-14 19:54:58 -0500

    #1 Se agrega relaciones con user, teacher, laboratory, career, asi como sus atributos

--- rr_web/grails-app/domain/mx/uaemex/Bitacora.groovy
+++ rr_web/grails-app/domain/mx/uaemex/Bitacora.groovy
@@ -2,6 +2,17 @@ package mx.uaemex
 
 class Bitacora {
 
-    static constraints = {
-    }
+	static constraints = {
+		resourcePicture(nullable:true)
+		groupName(nullable:true)
+
+	}
+
+	Date dateCreated 
+	Date lastUpdated
+	String groupName
+	String resourcePicture
+
+	static belongsTo = [user:User, teacher:Teacher, laboratory:Laboratory, career:Career]
+
 }
