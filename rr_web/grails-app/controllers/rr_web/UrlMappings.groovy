e208638c6529720bb47534f4afefe14c3b08e02c
Francisco Reyes <francisco@makingdevs.com>
2016-08-15 17:29:58 -0500

    #3 Se exponen recurso de bitacora como usuario

--- rr_web/grails-app/controllers/rr_web/UrlMappings.groovy
+++ rr_web/grails-app/controllers/rr_web/UrlMappings.groovy
@@ -12,5 +12,7 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
+        "/bitacoras"(resources:'bitacora')
+        "/users"(resources:'user')
     }
 }
