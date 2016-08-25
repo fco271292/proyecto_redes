0b221441d34f23c087d68eb0da5069a446b9a140
Francisco Reyes <francisco@makingdevs.com>
2016-08-25 12:12:33 -0500

    #4 Se agrega usuario por default

--- rr_web/grails-app/init/BootStrap.groovy
+++ rr_web/grails-app/init/BootStrap.groovy
@@ -1,7 +1,24 @@
+import mx.uaemex.*
+import grails.util.Environment
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+		createUser("user","user","user@gmail.com","ROLE_USER")
+		createUser("admin","admin","admin@gmail.com","ROLE_ADMIN")
+	}
+
+	def destroy = {
+	}
+
+	def createUser(String username, String password,String email, String authority ){
+		Role role = new Role(authority).save()
+		User user = new User(username:username, password:password, email:email, name:'name',lastName:'lastname').save()
+		UserRole.create user, role
+		UserRole.withSession {userRole->
+			userRole.flush()
+			userRole.clear()
+		}
+	}
+
 }
