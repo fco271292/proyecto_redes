7788d619127ff4c5f0a3df2c75d799ad95420abc
Francisco Reyes <francisco@makingdevs.com>
2016-08-25 12:10:16 -0500

    #4 Se ajusta la clase User para que soprte la seguridad de spring

--- rr_web/grails-app/domain/mx/uaemex/User.groovy
+++ rr_web/grails-app/domain/mx/uaemex/User.groovy
@@ -1,23 +1,67 @@
 package mx.uaemex
 
-class User {
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
 
-	static constraints = {
-		name(blank: false)
-		lastName(blank: false)
-		email(blank: false, email: true)
-		username(size: 3..20)
-		password(size: 3..20)
+@EqualsAndHashCode(includes='username')
+@ToString(includes='username', includeNames=true, includePackage=false)
+class User implements Serializable {
 
-	}
+	private static final long serialVersionUID = 1
 
-	Date dateCreated 
-	Date lastUpdated 
+	transient springSecurityService
+
+	String username
+	String password
+	boolean enabled = true
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
 	String name
 	String lastName
 	String email
-	String username
-	String password
+
+	User(String username, String password) {
+		this()
+		this.username = username
+		this.password = password
+	}
+
+	Set<Role> getAuthorities() {
+		UserRole.findAllByUser(this)*.role
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+	}
+
+	protected void encodePassword() {
+		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+	}
+
+	static transients = ['springSecurityService']
+
+	static constraints = {
+		username blank: false, unique: true
+		password blank: false
+		name(blank: true, nullable:true)
+		lastName(blank: true,nullable:true)
+		email(blank: false, email: true)
+		enabled nullable:true
+		accountExpired nullable:true
+		accountLocked nullable:true
+		passwordExpired nullable:true
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
 
 	static hasMany = [bitacoras : Bitacora]
 	
