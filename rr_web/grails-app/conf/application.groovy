c5b660975535dbdd543f03c5d048880f63259d18
Francisco Reyes <francisco@makingdevs.com>
2016-08-25 12:13:31 -0500

    #4 Se agrega por default de la seguridad de URL con spring

---
+++ rr_web/grails-app/conf/application.groovy
@@ -0,0 +1,38 @@
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'mx.uaemex.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'mx.uaemex.UserRole'
+grails.plugin.springsecurity.authority.className = 'mx.uaemex.Role'
+grails.plugin.springsecurity.rejectIfNoRule = true
+grails.plugin.springsecurity.fii.rejectPublicInvocations = true
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/login/**',       access: ['permitAll']],
+  [pattern: '/logout/**',      access: ['permitAll']],
+	[pattern: '/user/**',        access: 'permitAll'],
+	[pattern: '/bitacora/**',        access: 'IS_AUTHENTICATED_FULLY'],
+	[pattern: '/career/**',        access: 'ROLE_ADMIN'],
+	[pattern: '/laboratoty/**',        access: 'ROLE_ADMIN'],
+	[pattern: '/resource/**',        access: 'ROLE_ADMIN'],
+	[pattern: '/teacher/**',        access: 'ROLE_ADMIN']
+
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/**/js/**',       filters: 'none'],
+	[pattern: '/**/css/**',      filters: 'none'],
+	[pattern: '/**/images/**',   filters: 'none'],
+	[pattern: '/**/favicon.ico', filters: 'none'],
+	[pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
