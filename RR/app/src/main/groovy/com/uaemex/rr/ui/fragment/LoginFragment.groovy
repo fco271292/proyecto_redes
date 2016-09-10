2a6aef2881d3718a5c373586d9a474a48bb333f3
Francisco Reyes <francisco@makingdevs.com>
2016-09-10 00:15:17 -0500

    #12 Se agrega metodo para unir elementos con la vista

--- RR/app/src/main/groovy/com/uaemex/rr/ui/fragment/LoginFragment.groovy
+++ RR/app/src/main/groovy/com/uaemex/rr/ui/fragment/LoginFragment.groovy
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.uaemex.rr.R
 import groovy.transform.CompileStatic
 
@@ -15,19 +16,15 @@ import groovy.transform.CompileStatic
 class LoginFragment extends Fragment {
 
     FloatingActionButton mFloatingActionButtonRegistration
+    Button mButtonLogin
 
     @Override
     View onCreateView(LayoutInflater inflater,
                       @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState)
         View view = inflater.inflate(R.layout.fragment_login, container, false)
-
-        mFloatingActionButtonRegistration = (FloatingActionButton) view.findViewById(R.id.fabAddUser)
-        mFloatingActionButtonRegistration.onClickListener = {
-           changeFragment(new RegistrationFragment())
-        }
+        bindingWidgets(view)
         view
-
     }
 
     void changeFragment(Fragment fragment) {
@@ -35,5 +32,16 @@ class LoginFragment extends Fragment {
                 .replace(((ViewGroup) getView().getParent()).getId(), fragment)
                 .addToBackStack(null)
                 .commit()
+    }
+
+    void bindingWidgets(View view) {
+        mFloatingActionButtonRegistration = (FloatingActionButton) view.findViewById(R.id.fabAddUser)
+        mFloatingActionButtonRegistration.onClickListener = {
+            changeFragment(new RegistrationFragment())
+        }
+        mButtonLogin = view.findViewById(R.id.buttonLogin) as Button
+        mButtonLogin.onClickListener = {
+            changeFragment(new BitacoraFragment())
+        }
     }
 }
