832ef165e86b8b9d9662527e17dbdf64dbac2e75
Francisco Reyes <francisco@makingdevs.com>
2016-09-12 16:30:33 -0500

    #13 Se agrega implementacion de Retrofit2, ademas de tomar los valores de los textbox para consumir servicio

--- RR/app/src/main/groovy/com/uaemex/rr/ui/fragment/LoginFragment.groovy
+++ RR/app/src/main/groovy/com/uaemex/rr/ui/fragment/LoginFragment.groovy
@@ -4,19 +4,29 @@ import android.os.Bundle
 import android.support.annotation.Nullable
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import com.uaemex.rr.R
+import com.uaemex.rr.api.client.FabricRRWebClient
+import com.uaemex.rr.api.client.GitHubClient
+import com.uaemex.rr.api.model.UserGithubDetail
 import groovy.transform.CompileStatic
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 @CompileStatic
 class LoginFragment extends Fragment {
 
     FloatingActionButton mFloatingActionButtonRegistration
     Button mButtonLogin
+    EditText mEditTextUsername
+    EditText mEditTextPassword
+    String username
+    String password
 
     @Override
     View onCreateView(LayoutInflater inflater,
@@ -41,7 +51,30 @@ class LoginFragment extends Fragment {
         }
         mButtonLogin = view.findViewById(R.id.buttonLogin) as Button
         mButtonLogin.onClickListener = {
-            changeFragment(new BitacoraFragment())
+            //changeFragment(new BitacoraFragment())
+            GitHubClient client = FabricRRWebClient.createService(GitHubClient)
+            populateForm()
+            client.getUserGithub(username).enqueue(new Callback<UserGithubDetail>() {
+                @Override
+                void onResponse(Call<UserGithubDetail> call, Response<UserGithubDetail> response) {
+                    println("*"*100)
+                    println(response.body().dump())
+                }
+
+                @Override
+                void onFailure(Call<UserGithubDetail> call, Throwable t) {
+                    println("-"*100)
+                    println("${t?.message}")
+                }
+            })
         }
+        mEditTextUsername = view.findViewById(R.id.username) as EditText
+        mEditTextPassword = view.findViewById(R.id.password) as EditText
+
+    }
+
+    void populateForm(){
+        username = mEditTextUsername.text
+        password = mEditTextPassword.text
     }
 }
