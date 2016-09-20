a6cef6922c236b2ed18bc82c3c694d15182f3281
Francisco Reyes <francisco@makingdevs.com>
2016-09-20 17:28:29 -0500

    #16 Se agrega modelo User que sera parseado la respuesta

---
+++ RR/app/src/main/groovy/com/uaemex/rr/api/model/User.groovy
@@ -0,0 +1,19 @@
+package com.uaemex.rr.api.model
+
+import com.google.gson.annotations.SerializedName
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class User {
+    @SerializedName("name")
+    String name
+    @SerializedName("lastName")
+    String lastName
+    @SerializedName("email")
+    String email
+    @SerializedName("username")
+    String username
+    @SerializedName("password")
+    String password
+
+}
