beb76cf1fb38c7fd456b7cc544bc43e6bb3a0559
Francisco Reyes <francisco@makingdevs.com>
2016-09-23 14:52:52 -0500

    #18 Se agrega modelo bitacora

---
+++ RR/app/src/main/groovy/com/uaemex/rr/api/model/command/BitacoraCommand.groovy
@@ -0,0 +1,14 @@
+package com.uaemex.rr.api.model.command
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class BitacoraCommand {
+
+    String groupName
+    String user
+    String teacher
+    String laboratory
+    String career
+
+}
