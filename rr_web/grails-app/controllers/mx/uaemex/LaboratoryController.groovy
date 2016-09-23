307379461cf11a8cb6e48f5d25b7432ee4391b92
Francisco Reyes <francisco@makingdevs.com>
2016-09-23 14:32:36 -0500

    #17 Se agrega metodo para laboratorios como JSON

--- rr_web/grails-app/controllers/mx/uaemex/LaboratoryController.groovy
+++ rr_web/grails-app/controllers/mx/uaemex/LaboratoryController.groovy
@@ -2,7 +2,10 @@ package mx.uaemex
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
+import grails.plugin.springsecurity.annotation.Secured
+import grails.converters.*
 
+@Secured(['ROLE_USER','ROLE_ADMIN'])
 @Transactional(readOnly = true)
 class LaboratoryController {
 
@@ -93,6 +96,10 @@ class LaboratoryController {
             }
             '*'{ render status: NO_CONTENT }
         }
+    }
+
+    def list(){
+        render Laboratory.list() as JSON
     }
 
     protected void notFound() {
