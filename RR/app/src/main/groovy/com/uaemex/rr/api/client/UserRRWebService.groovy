d3d4d6584ef9e31174446c969dccaeaaa118683b
Francisco Reyes <francisco@makingdevs.com>
2016-09-23 14:42:21 -0500

    #18 Se agrega endpoint para obtener usuario como JSON

--- RR/app/src/main/groovy/com/uaemex/rr/api/client/UserRRWebService.groovy
+++ RR/app/src/main/groovy/com/uaemex/rr/api/client/UserRRWebService.groovy
@@ -7,8 +7,10 @@ import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 @CompileStatic
 interface UserRRWebService {
@@ -24,5 +26,12 @@ interface UserRRWebService {
             @Field("password") String password
 
     )
+
+    @GET("/user")
+    Call<User> getUserByUsername(
+            @Header("Authorization") String authorization,
+            @Query("username") String username
+    )
+
 
 }
