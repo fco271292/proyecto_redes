6df905efc91851cd9a437f2e206142cdb7d77a60
Francisco Reyes <francisco@makingdevs.com>
2016-09-26 22:46:05 -0500

    #20 Se agrega test por default de Document

---
+++ rr_web/src/test/groovy/mx/uaemex/DocumentControllerSpec.groovy
@@ -0,0 +1,22 @@
+package mx.uaemex
+
+import grails.test.mixin.TestFor
+import spock.lang.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DocumentController)
+class DocumentControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+		@Ignore
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
