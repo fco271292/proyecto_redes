20cef736cbe6d6b998c4efbf8bbb59cee63903e9
Francisco Reyes <francisco@makingdevs.com>
2016-09-28 14:46:41 -0500

    #22 Se agrega relacion con documento

--- rr_web/grails-app/domain/mx/uaemex/Bitacora.groovy
+++ rr_web/grails-app/domain/mx/uaemex/Bitacora.groovy
@@ -3,15 +3,15 @@ package mx.uaemex
 class Bitacora {
 
 	static constraints = {
-		groupName(nullable:true)
-		resourcePicture(nullable:true)
+		groupName nullable:true
+		document nullable:true
 	}
 
 	Date dateCreated 
 	Date lastUpdated
 	String groupName
-	String resourcePicture
-
+	Document document
+	
 	static belongsTo = [user:User, teacher:Teacher, laboratory:Laboratory, career:Career]
 
 }
