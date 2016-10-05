3e11c41ca277fa39f6e238db938a31f026960a70
Francisco Reyes <francisco@makingdevs.com>
2016-10-05 09:57:29 -0500

    #29 Se agrega metodo para generar PDF con el plugin Export

--- rr_web/grails-app/controllers/mx/uaemex/BitacoraController.groovy
+++ rr_web/grails-app/controllers/mx/uaemex/BitacoraController.groovy
@@ -2,11 +2,14 @@ package mx.uaemex
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
+import java.awt.Color
 
 @Transactional(readOnly = true)
 class BitacoraController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def exportService
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
@@ -102,6 +105,22 @@ class BitacoraController {
                 redirect action: "index", method: "GET"
             }
             '*'{ render status: NOT_FOUND }
+        }
+    }
+
+    def list = {
+        if(!params.max) params.max = 10
+
+        if(params?.extension && params.extension != "html"){
+            response.contentType = grailsApplication.config.grails.mime.types[params.extension]
+            response.setHeader("Content-disposition", "attachment; filename=Bitacora_${new Date().format('dd_MM_yyyy')}.${params.extension}")
+
+            List fields = ["id", "groupName","document","user","teacher","laboratory","career","dateCreated"]
+            Map labels = [id: "ID", groupName: "Grupo", document: "Fotografía", user: "Usuario", teacher: "Docente", laboratory: "Laboratorio", career: "Carrera", dateCreated: "Fecha de inicio"]
+            Map parameters = [title: "Bitacora","pdf.encoding": "UTF-8","title.font.size": 14, "title.font.style": "bold","separator.color": new Color(255, 255, 255) ]
+            def bitacoraList = Bitacora.findAllWhere(user:9 as User)
+            
+            exportService.export(params.extension, response.outputStream, bitacoraList, fields, labels, [:] , parameters)
         }
     }
 }
